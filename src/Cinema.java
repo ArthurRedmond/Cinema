8a89a8e9caf79442d1976b45779abab0ba0c0746
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-03-26 15:14:37 -0400

    Updated with booked seat

--- src/Cinema.java
+++ src/Cinema.java
@@ -4,14 +4,76 @@ public class Cinema {
 
     public static void main(String[] args) {
 
-
-        printSeatMap();
-
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int numRows = s.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int numSeats = s.nextInt();
+        System.out.println("");
+
+        printSeatMap(numRows, numSeats);
+        System.out.println("");
+
+        System.out.println("Enter a row number:");
+        int seatRow = s.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seatNumber = s.nextInt();
+
+        System.out.println("\nTicket price: $" + ticketPrice(numRows, numSeats, seatRow));
+        printSeatMap(numRows, numSeats, seatRow, seatNumber);
+    }
+
+    public static void printSeatMap(int rows, int seats) {
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for (int i = 1; i <= seats; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println("");
+        for (int i = 1; i <= rows; i++) {
+            String str = "S ";
+            System.out.print(i + " ");
+            System.out.println(str.repeat(seats));
+        }
+    }
+
+    public static void printSeatMap(int rows, int seats, int seatRow, int seatNumber) {
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for (int i = 1; i <= seats; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println("");
+        for (int i = 1; i <= rows; i++) {
+            String str = "S ";
+            if (i == seatRow) {
+                System.out.print(i + " ");
+                System.out.println(str.repeat(seatNumber - 1) + "B " + str.repeat(seats - seatNumber));
+            } else {
+                System.out.print(i + " ");
+                System.out.println(str.repeat(seats));
+            }
+        }
+    }
+
+    public static int ticketPrice(int numRows, int numSeats, int seatRow) {
+        int totalSeats = numSeats * numRows;
+        int numFrontRows = numRows / 2;
+        int ticketPrice = 0;
+
+        if (totalSeats <= 60) {
+            ticketPrice = 10;
+        } else {
+            if (seatRow <= numFrontRows) {
+                ticketPrice = 10;
+            } else {
+                ticketPrice = 8;
+            }
+        }
+        return ticketPrice;
+    }
+
+    public static void printTotalIncome(int numRows, int numSeats) {
 
         int totalSeats = numSeats * numRows;
         int numFrontRows = numRows / 2;
@@ -30,20 +92,5 @@ public class Cinema {
         System.out.println("Total income:");
         System.out.println("$" + totalIncome);
     }
-
-    public static void printSeatMap() {
-        int rows = 7;
-        int seats = 8;
-        System.out.println("Cinema:");
-        System.out.print(" ");
-        for (int i = 0; i < seats; i++) {
-            System.out.print(" " + i);
-        }
-        System.out.println("");
-        for (int i = 1; i < rows; i++) {
-            String str = "S ";
-            System.out.print(i + " ");
-            System.out.println(str.repeat(seats));
-        }
-    }
 }
+
